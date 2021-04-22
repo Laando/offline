18ee759cd6e18936b80853f7b1d3c7895b55287d
Laando <tienlando2704@gmail.com>
2021-04-23 00:17:29 +0700

    udpate demo

--- sw.js
+++ sw.js
@@ -10,7 +10,6 @@ var filesToCache = [
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      console.log(cacheName);
       return cache.addAll(filesToCache);
     })
   );
