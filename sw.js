65d9e9e0ee334abaf3bcc821a1319fc30b3bf1f0
Laando <43564422+Laando@users.noreply.github.com>
2021-04-22 23:54:35 +0700

    aa

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 var cacheName = 'hello-pwa';
 var filesToCache = [
-  '/offline',
-  '/offline/index.html',
-  '/offline/css/style.css',
-  '/offline/js/main.js'
+  '/',
+  './index.html',
+  './css/style.css',
+  './js/main.js'.
 ];
 
 /* Start the service worker and cache all of the app's content */
