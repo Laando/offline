d90a31f0cdf8555789727e52094e0fbcdb916d0a
Laando <tienlando2704@gmail.com>
2021-04-23 20:24:07 +0700

    update

--- sw.js
+++ sw.js
@@ -1,15 +1,17 @@
-var cacheName = 'hello';
+// var cacheName = 'LaanDo';
+const staticCacheName = 'site-static-v3';
+const dynamicCacheName = 'site-dynamic-v3';
 var filesToCache = [
   '/',
-  './index.html',
-  './css/style.css',
-  './js/main.js'
+  '/offline/index.html',
+  '/offline/css/style.css',
+  '/offline/js/main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open(staticCacheName).then(function(cache) {
       return cache.addAll(filesToCache);
     })
   );
@@ -23,3 +25,45 @@ self.addEventListener('fetch', function(e) {
     })
   );
 });
+
+// install event
+// self.addEventListener('install', evt => {
+//   evt.waitUntil(
+//     caches.open(staticCacheName).then((cache) => {
+//       console.log(staticCacheName);
+//       console.log('Khoi tao thanh cong');
+//       cache.addAll(filesToCache);
+//     })
+//   );
+// });
+
+// activate event
+// self.addEventListener('activate', evt => {
+//   console.log('Xay dung');
+//   evt.waitUntil(
+//     caches.keys().then(keys => {
+//       console.log(keys);
+//       return Promise.all(keys
+//         .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+//         .map(key => caches.delete(key))
+//       );
+//     })
+//   );
+// });
+
+// fetch event
+// self.addEventListener('fetch', evt => {
+//   // console.log('fetch event', evt);
+//   evt.respondWith(
+//     caches.match(evt.request).then(cacheRes => {
+//       console.log('fetch event', evt);
+//       return cacheRes || fetch(evt.request).then(fetchRes => {
+//         return caches.open(dynamicCacheName).then(cache => {
+//           if(filesToCache.includes(evt.request.url))
+//             cache.put(evt.request.url, fetchRes.clone());
+//           return fetchRes;
+//         })
+//       });
+//     })
+//   );
+// });
